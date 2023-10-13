ec6348432c372d472e17bc55715902a90b5ed637
Владимир Кулаков <VrachKulakovVS@mail.ru>
2023-10-13 22:33:59 +0300

    flake8

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -10,15 +10,15 @@ class Category(PublishedModel):
     title = models.CharField(
         max_length=256,
         verbose_name='Заголовок'
-        )
+    )
     description = models.TextField(verbose_name='Описание')
     slug = models.SlugField(
         max_length=64,
         unique=True,
         verbose_name='Идентификатор',
-        help_text='Идентификатор страницы для URL; ' +
-        'разрешены символы латиницы, цифры, дефис и подчёркивание.'
-        )
+        help_text='Идентификатор страницы для URL; '
+        + 'разрешены символы латиницы, цифры, дефис и подчёркивание.'
+    )
 
     class Meta:
         verbose_name = 'категория'
@@ -32,7 +32,7 @@ class Location(PublishedModel):
     name = models.CharField(
         max_length=256,
         verbose_name='Название места'
-        )
+    )
 
     class Meta:
         verbose_name = 'местоположение'
@@ -47,27 +47,27 @@ class Post(PublishedModel):
     text = models.TextField(verbose_name='Текст')
     pub_date = models.DateTimeField(
         verbose_name='Дата и время публикации',
-        help_text='Если установить дату и время в будущем' +
-        ' — можно делать отложенные публикации.'
-        )
+        help_text='Если установить дату и время в будущем'
+        + ' — можно делать отложенные публикации.'
+    )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         verbose_name='Автор публикации'
-        )
+    )
     location = models.ForeignKey(
         Location,
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
         verbose_name='Местоположение'
-        )
+    )
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
         null=True,
         verbose_name='Категория'
-        )
+    )
 
     class Meta:
         verbose_name = 'публикация'
