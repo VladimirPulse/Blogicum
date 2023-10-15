a196328af39646ab047ff72673f725f004bbff8b
Владимир Кулаков <VrachKulakovVS@mail.ru>
2023-10-15 11:26:36 +0300

    pravka

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -12,25 +12,35 @@ def posts():
         'author',
         'location',
         'category'
-    ).filter(
-        pub_date__lte=datetime.now(),
-        is_published=True,
-        category__is_published=True
-    ).order_by('-pub_date')
+    )
+    # ).filter(
+    #     pub_date__lte=datetime.now(),
+    #     is_published=True,
+    #     category__is_published=True
+    # ).order_by('-pub_date')
     return posts
 
 
 def index(request):
     return render(request,
                   'blog/index.html',
-                  {'post_list': posts()[:PUBLISH_POSTS]}
+                  {'post_list': posts().filter(
+                    pub_date__lte=datetime.now(),
+                    is_published=True,
+                    category__is_published=True
+                    ).order_by('-pub_date')[:PUBLISH_POSTS]}
                   )
 
 
 def post_detail(request, post_id):
     return render(request,
                   'blog/detail.html',
-                  {'post': get_object_or_404(posts(), pk=post_id)}
+                  {'post': get_object_or_404(
+                      Post,
+                      pk=post_id,
+                      pub_date__lte=datetime.now(),
+                      is_published=True,
+                      category__is_published=True)}
                   )
 
 
@@ -39,6 +49,70 @@ def category_posts(request, category_slug):
                                  slug=category_slug,
                                  is_published=True
                                  )
-    post_list = posts().filter(category_id=category.pk)
+    post_list = posts().filter(
+        category=category,
+        is_published=True,
+        pub_date__lte=datetime.now()
+    )
     context = {'category': category, 'post_list': post_list}
     return render(request, 'blog/category.html', context)
+
+# from django.db.models.functions import Now
+# from django.shortcuts import get_object_or_404, render
+
+# from .models import Category, Post
+# from .constants import POSTS_LIMIT
+
+
+
+# def index(request):
+#     template_name = 'blog/index.html'
+#     posts = Post.objects.select_related('category').filter(
+#         pub_date__lte=timezone.now(),
+#         is_published=True,
+#         category__is_published=True,
+#     ).order_by('-pub_date')[:POSTS_LIMIT]
+
+#     context = {
+#         'post_list': posts
+#     }
+
+#     return render(request, template_name, context)
+
+
+# def post_detail(request, post_id):
+#     template_name = 'blog/detail.html'
+#     post = get_object_or_404(
+#         Post,
+#         pk=post_id,
+#         pub_date__lte=timezone.now(),
+#         is_published=True,
+#         category__is_published=True,
+#     )
+
+#     context = {
+#         'post': post
+#     }
+
+#     return render(request, template_name, context)
+
+
+# def category_posts(request, category_slug):
+#     template_name = 'blog/category.html'
+#     category = get_object_or_404(
+#         Category,
+#         slug=category_slug,
+#         is_published=True
+#     )
+#     posts = Post.objects.filter(
+#         category=category,
+#         is_published=True,
+#         pub_date__lte=timezone.now()
+#     )
+
+#     context = {
+#         'category': category,
+#         'post_list': posts
+#     }
+
+#     return render(request, template_name, context)
