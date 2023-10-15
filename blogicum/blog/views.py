e4fe5b81f0b8ac79072c269d4e7c3df1d0508f82
Владимир Кулаков <VrachKulakovVS@mail.ru>
2023-10-15 12:30:56 +0300

    pravka_5

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,6 +1,6 @@
-from datetime import datetime
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 
-from django.shortcuts import render, get_object_or_404
+from datetime import datetime
 
 from blog.models import Post, Category
 
@@ -12,43 +12,30 @@ def posts():
         'author',
         'location',
         'category'
-    )
+    ).filter(
+        pub_date__lte=datetime.now(),
+        is_published=True,
+        category__is_published=True
+    ).order_by('-pub_date')
     return posts
 
 
 def index(request):
-    return render(
-        request,
-        'blog/index.html',
-        {'post_list': posts().filter(
-            pub_date__lte=datetime.now(),
-            is_published=True,
-            category__is_published=True
-        ).order_by('-pub_date')[:PUBLISH_POSTS]}
-    )
+    return render(request,
+                  'blog/index.html',
+                  {'post_list': posts()[:PUBLISH_POSTS]}
+                  )
 
 
 def post_detail(request, post_id):
     return render(request,
                   'blog/detail.html',
-                  {'post': get_object_or_404(
-                      Post,
-                      pk=post_id,
-                      pub_date__lte=datetime.now(),
-                      is_published=True,
-                      category__is_published=True)}
+                  {'post': get_object_or_404(posts(), pk=post_id)}
                   )
 
 
 def category_posts(request, category_slug):
-    category = get_object_or_404(Category,
-                                 slug=category_slug,
-                                 is_published=True
-                                 )
-    post_list = posts().filter(
-        category=category,
-        is_published=True,
-        pub_date__lte=datetime.now()
-    )
+    category = get_object_or_404(Category, slug=category_slug)
+    post_list = get_list_or_404(posts(), category_id=category.pk)
     context = {'category': category, 'post_list': post_list}
     return render(request, 'blog/category.html', context)
